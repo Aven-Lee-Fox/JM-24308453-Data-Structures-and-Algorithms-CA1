852b2c12a95e39720cca6ec05f1b5a63ea3632af
Joseph Moiselle <moisellejoseph05@gmail.com>
2026-03-18 00:21:33 +0000

    Netbeans project created, main GUI and both versions of project queue created, started login function

---
+++ JM-24308453-DSA-CA1-App/src/main/java/com/josephmoiselle/jm/dsa/ca1/app/JM24308453DSACA1App.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.josephmoiselle.jm.dsa.ca1.app;
+
+/**
+ *
+ * @author Joseph Moiselle 24308453
+ */
+public class JM24308453DSACA1App {
+
+    public static void main(String[] args) {
+        MainGUI MainPage = new MainGUI();
+        MainPage.setVisible(true);
+        boolean admin = false;
+        
+    }
+    
+}
---
+++ JM-24308453-DSA-CA1-App/src/main/java/com/josephmoiselle/jm/dsa/ca1/app/loginStatus.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.josephmoiselle.jm.dsa.ca1.app;
+
+/**
+ *
+ * @author Joseph Moiselle 24308453
+ */
+public class loginStatus {
+    
+    boolean status = false;
+    
+    public boolean getStatus(){
+    
+        return status;
+        
+    }
+    
+    public void setStatus(boolean status){
+    
+        this.status = status;
+        
+    }
+    
+}
